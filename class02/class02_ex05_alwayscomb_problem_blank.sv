623639e745b4eb5449f4bc58f96c308ba704311e
jongsup-baek <jongsup.baek@ksdcsemi.com>
2026-03-06 13:28:57 +0900

    fix: ex05 blank — logic out + always 전체 blank

--- class02/class02_ex05_alwayscomb_problem_blank.sv
+++ class02/class02_ex05_alwayscomb_problem_blank.sv
@@ -13,74 +13,57 @@ module ablock (
 
    // Comment #1 : Verilog-Style
    //    a variable to multiple procedures
-   logic out;
-
 //////////////////////////////////////////////////////////
 // 여기에 적으세요. (시작)
-// HINT: always_comb 대신 적절한 감도 리스트를 작성하세요
+// HINT: demo는 always_comb로 out에 multiple driver 에러 발생 — Verilog 방식 always @(감도리스트)로 수정하세요
 //////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////
 // 여기까지 입니다. (끝)
 //////////////////////////////////////////////////////////
-         out= a;
-      else
-         out= b;
-
-//////////////////////////////////////////////////////////
-// 여기에 적으세요. (시작)
-// HINT: always_comb 대신 적절한 감도 리스트를 작성하세요
-//////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////
-// 여기까지 입니다. (끝)
-//////////////////////////////////////////////////////////
-         out= c;
-      else
-         out= d;
 
 endmodule
 
 module tb();
-	
+
 	logic a_in, b_in, c_in, d_in;
 	logic sel, sel2;
 	logic y_out;
-	
-	ablock u_duta( 
-	        .y(y_out), .sel(sel), .sel2(sel2), 
+
+	ablock u_duta(
+	        .y(y_out), .sel(sel), .sel2(sel2),
 	        .a(a_in), .b(b_in), .c(c_in), .d(d_in));
-	
+
 	initial begin
 	   a_in = 0;
-	   b_in = 0; 
+	   b_in = 0;
 	   c_in = 0;
-	   d_in = 0; 
+	   d_in = 0;
 	   sel  = 0;
 	   sel2 = 0;
 	#10;
 	   a_in = 0;
-	   b_in = 1; 
+	   b_in = 1;
 	   c_in = 0;
-	   d_in = 0; 
+	   d_in = 0;
 	   sel  = 0;
 	   sel2 = 0;
 	#10;
 	   a_in = 1;
-	   b_in = 0; 
+	   b_in = 0;
 	   c_in = 0;
-	   d_in = 0; 
+	   d_in = 0;
 	   sel  = 0;
 	   sel2 = 0;
 	#10;
 	   a_in = 1;
-	   b_in = 1; 
+	   b_in = 1;
 	   c_in = 0;
-	   d_in = 0; 
+	   d_in = 0;
 	   sel  = 0;
 	   sel2 = 0;
 	#10;
 	   $finish;
 	end
-	
+
 endmodule
